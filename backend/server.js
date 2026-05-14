3f38a0a3dbf54fafcb589fa301448737d4801eaa
hanibenkacem <hanibenkacem007@gmail.com>
2026-05-14 01:52:46 +0100

    die

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ require("./db/db");
 
 // middlewares
 app.use(cors({
-  origin: 'https://shop-bike-eight.vercel.app', // this one is permanent
+  origin: (origin, callback) => {
+    if (!origin || origin.endsWith('.vercel.app')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
